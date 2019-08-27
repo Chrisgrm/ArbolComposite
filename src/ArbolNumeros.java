6c904c728aaae1a6a740b1085faac8ca7d2f5ac5
Chrisgrm <42417217+Chrisgrm@users.noreply.github.com>
2019-08-27 17:24:05 -0500

    Add files via upload

---
+++ src/ArbolNumeros.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+import java.util.Optional;
+
+/**
+ *
+ * @author chris
+ */
+public abstract class ArbolNumeros {
+    
+//    Integer value;
+    
+    public abstract void add(Integer value);
+    
+    public abstract int getValor();
+     
+    public abstract Optional<Node> find(Integer value);
+     
+    public abstract void printInOrder();
+     
+    public abstract void printPreOrder();    
+ 
+    public abstract void printPosOrder();
+    
+    public abstract String toString();
+}
+    
+
